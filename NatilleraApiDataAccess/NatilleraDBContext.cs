9f07abb3a1b5020641383588bfdae96886a96c12
darwinum <dun34@hotmail.com>
2019-09-09 16:07:23 -0500

    se agrega core sql y la clase de contexto.

---
+++ NatilleraApiDataAccess/NatilleraDBContext.cs
@@ -0,0 +1,19 @@
+﻿namespace NatilleraApiDataAccess
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class NatilleraDBContext : DbContext
+    {
+        public NatilleraDBContext()
+        {
+                
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
