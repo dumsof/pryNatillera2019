a0b84b31d626cb044ffab92dddc5b475c54bfb7e
darwinum <dun34@hotmail.com>
2019-09-25 13:36:47 -0500

    se agregan los json para el manejo de mensajes  de negocio

---
+++ NotificacionApiCrossClothing/Utilidades/ManejoMensajes.cs
@@ -0,0 +1,23 @@
+﻿namespace NotificacionApiCrossClothing.Utilidades
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+
+
+    public class ManejoMensajes
+    {
+        public ManejoMensajes()
+        {
+            //var configuration = new ConfigurationBuilder()
+            //             .AddEnvironmentVariables()
+            //             .AddJsonFile("MensajeNegocio.json", true)
+            //             .Build();
+        }
+
+        private void LeerArchivo()
+        {
+            //var configuracion = new ConfigurationBuilder()
+        }
+       
+    }
+}
