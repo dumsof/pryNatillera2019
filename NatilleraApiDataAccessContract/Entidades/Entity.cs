7c636b3567c2a031be724929b9e45c3c6f797843
darwinum <dun34@hotmail.com>
2019-10-09 11:13:54 -0500

    se agrega la propiedad para el control de concurrencia de forma centralizada.

---
+++ NatilleraApiDataAccessContract/Entidades/Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace NatilleraApiDataAccessContract.Entidades
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Entity
+    {
+        /// <summary>
+        /// controlar la concurrencia.
+        /// </summary>
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
--- NatilleraApiDataAccessContract/Entidades/NatilleraEntity.cs
+++ NatilleraApiDataAccessContract/Entidades/NatilleraEntity.cs
@@ -5,19 +5,13 @@ namespace NatilleraApiDataAccessContract.Entidades
     using System.ComponentModel.DataAnnotations;
 
     //clase para mapear los campos de la base de datos.
-    public class NatilleraEntity
+    public class NatilleraEntity: Entity
     {
         [Key]
         public int IdNatillera { get; set; }
 
         public string Nombre { get; set; }
 
-        public string Descripcion { get; set; }
-
-        /// <summary>
-        /// controlar la concurrencia.
-        /// </summary>
-        [Timestamp]
-        public byte[] RowVersion { get; set; }
+        public string Descripcion { get; set; }      
     }
 }
